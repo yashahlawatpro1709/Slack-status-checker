48162634ad2e511b91520eb5c6dfbe419c744918
Yash Ahlawat <129209985+yashahlawatpro1709@users.noreply.github.com>
2025-05-08 14:35:39 +0530

    Update SlackStatusCheckerIT.java

--- src/test/java/com/slacknotifier/SlackStatusCheckerIT.java
+++ src/test/java/com/slacknotifier/SlackStatusCheckerIT.java
@@ -1,8 +1,1 @@
-package com.slacknotifier;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class SlackStatusCheckerIT extends SlackStatusCheckerTest {
-    // Execute the same tests but in packaged mode.
-}
